025ec45502fbf457f036fed6a6b5dfd37ec424ed
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2021-10-08 08:49:25 +0300

    modified server.js

--- ReplicatedLogSecondary/server.js
+++ ReplicatedLogSecondary/server.js
@@ -1,6 +1,5 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const port = 3000;
 
@@ -9,23 +8,16 @@ app.use(bodyParser.json());
 
 const messages = [];
 
-io.on('connection', (socket) => {
-
-  app.get('/', (req, res) => {
-    console.log('Client connected');
-    res.send('Hello from Server!');
-  })
-
-  app.post('/', function(req, res) {
-    console.log(res.statusCode + ' Received: ' + req.body.message);
-    messages.push(req.body.message);
-    res.send(req.body.message);
-
-    io.emit('message', "All messages: " + messages);
-  })
-
+app.post('/', function(req, res) {
+  console.log(res.statusCode + ' Received: ' + req.body.message);
+  messages.push(req.body.message);
+  res.send(req.body.message);
 })
 
+app.get('/', (req, res) => {
+  console.log("All messages: " + messages);
+  res.send("All messages: " + messages);
+})
 
 http.listen(port, () => {
   console.log(`Server is listening on ${port}`);
