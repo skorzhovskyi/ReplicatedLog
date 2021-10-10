b4b6f2ec604bdfaecac9ab156891262d14650f98
Taras Shypka <tarashypka@gmail.com>
2021-10-10 20:38:49 +0300

    :sparkles: Implement second slave
    Implement endpoints for second slave

--- ReplicatedLogSecondary2/service.py
+++ ReplicatedLogSecondary2/service.py
@@ -1,20 +1,47 @@
 import os
+import sys
+from queue import Queue
 
-from flask import Flask
+import flask
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+messages = Queue()
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET'])
+def get_messages():
+    print('Get all messages ...', file=sys.stderr)
+    response = {'messages': [message for message in messages.queue]}
+    print(f'There are {messages.qsize()} messages in queue.', file=sys.stderr)
+
+    return flask.jsonify(response)
+
+
+@app.route("/", methods=['POST'])
+def append_message():
+
+    try:
+        request_body = flask.request.get_json()
+    except TypeError:
+        return flask.jsonify({'status': 'error', 'error': 'Could not parse input json!'})
+
+    try:
+        message = request_body['message']
+    except KeyError:
+        return flask.jsonify({'status': 'error', 'error': 'Not found message in input json!'})
+
+    print(f'Append [message="{message}"] ...', file=sys.stderr)
+    messages.put(message)
+    print(f'There are {messages.qsize()} messages in queue.', file=sys.stderr)
+
+    return flask.jsonify({'status': 'ok'})
 
 
 def main():
     host = os.environ['SLAVE_HOST']
     port = int(os.environ['SLAVE_PORT'])
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
 
 
 if __name__ == '__main__':
