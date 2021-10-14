ac43a10381fea849c4261a73ad958f4b2c66c120
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2021-10-14 21:56:12 +0300

    modified docker-compose and server.js

--- ReplicatedLogSecondary1/server.js
+++ ReplicatedLogSecondary1/server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const port = 3000;
+const DELAY = process.env.POST_DELAY;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -10,9 +11,12 @@ var messages = [];
 
 app.post('/', function(req, res) {
   console.log(res.statusCode + ' Secondary_1 received: ' + req.body.message);
-  messages.push(req.body.message);
+  setTimeout(function() { 
+    messages.push(req.body.message);
+  }, DELAY);
+
   res.send(req.body.message);
-})
+});
 
 app.get('/', (req, res) => {
   console.log("All messages: " + messages);
