9485f5eca3f05023e1772134e8bcc4f158be59cb
Taras Shypka <tarashypka@gmail.com>
2021-10-31 20:04:37 +0200

    :recycle: Refactor previous features
    Use seconds for POST_DELAY in first secondary

--- ReplicatedLogSecondary1/server.js
+++ ReplicatedLogSecondary1/server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const port = 3000;
-const DELAY = process.env.POST_DELAY;
+const POST_DELAY_MS = process.env.POST_DELAY * 1000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -14,7 +14,7 @@ app.post('/', function(req, res) {
 		console.log(res.statusCode + ' Secondary_1 received: ' + req.body.message);
 		messages.push(req.body.message);
 		res.send(req.body.message);
-	}, DELAY);
+	}, POST_DELAY_MS);
 });
 
 app.get('/', (req, res) => {
