895f51ef8006b0c0a4f918577aeeafdbb3e064a9
Taras Shypka <tarashypka@gmail.com>
2021-10-31 21:25:24 +0200

    :goal_net: Handle more errors in second secondary

--- ReplicatedLogSecondary2/service.py
+++ ReplicatedLogSecondary2/service.py
@@ -1,7 +1,6 @@
 
 import os
 import time
-import typing as _t
 from queue import Queue
 
 import flask
@@ -17,16 +16,16 @@ logger = get_console_logger('replicated-log-slave-2')
 post_delay = int(os.getenv('POST_DELAY', 0))
 
 
-def _get_error_response(body: _t.Dict) -> flask.Response:
+def _get_error_response(msg: str) -> flask.Response:
     """Log error response with stacktrace before return"""
-    logger.exception(body)
-    return flask.jsonify(body)
+    logger.exception(msg)
+    return flask.jsonify({'status': 'error', 'error': msg})
 
 
 @app.route("/", methods=['GET'])
 def get_messages():
     logger.info('Get all messages ...')
-    response = {'messages': [message for message in messages.queue]}
+    response = {'status': 'ok', 'messages': [message for message in messages.queue]}
     logger.info(f'There are {messages.qsize()} messages in queue.')
 
     return flask.jsonify(response)
@@ -42,12 +41,17 @@ def append_message():
     try:
         request_body = flask.request.get_json()
     except TypeError:
-        return _get_error_response({'status': 'error', 'error': 'Could not parse input json!'})
+        return _get_error_response('Could not parse input json!')
 
     try:
         message = request_body['message']
     except KeyError:
-        return _get_error_response({'status': 'error', 'error': 'Not found message in input json!'})
+        return _get_error_response('Not found "message" in input json!')
+    except TypeError:
+        return _get_error_response(
+            'Could not get data from request! '
+            'Possible reasons: missing Content-Type in headers.'
+        )
 
     logger.info(f'Append [message="{message}"] ...')
     messages.put(message)
