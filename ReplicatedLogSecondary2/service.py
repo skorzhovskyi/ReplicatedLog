1bf7d73e8f252c64672c16c95a0b5f5fb23f0da2
Taras Shypka <tarashypka@gmail.com>
2021-12-01 23:39:56 +0200

    :recycle: Write constants in upper case

--- ReplicatedLogSecondary2/service.py
+++ ReplicatedLogSecondary2/service.py
@@ -16,10 +16,20 @@ class ResponseStatus(Enum):
     ok = 'ok'
 
 
-service_name = 'replicated-log-secondary-2'
+SERVICE_NAME = 'replicated-log-secondary-2'
 
-app = flask.Flask(service_name)
-logger: _t.Optional[logging.Logger] = None
+# Delay in sec for POST requests, default is no delay
+POST_DELAY: _t.Optional[int] = None
+
+# This flag can be used to test messages consistency
+RETURN_ERROR_BEFORE_EVEN_MESSAGE = False
+
+# This flag can be used to test message deduplication
+RETURN_ERROR_AFTER_EVEN_MESSAGE = False
+
+LOGGER: _t.Optional[logging.Logger] = None
+
+app = flask.Flask(SERVICE_NAME)
 
 # Ready to display queue of messages
 messages_queue: _t.List[str] = []
@@ -28,15 +38,6 @@ messages_buffer: _t.Dict[int, str] = {}
 # Using lock on messages queue and buffer makes sure they are safe across multiple threads
 # NOTE: Can be acquired multiple times by the same thread
 messages_lock = RLock()
-
-# Delay in sec for POST requests, default is no delay
-post_delay: _t.Optional[int] = None
-
-# This flag can be used to test messages consistency
-return_error_before_even_message = False
-
-# This flag can be used to test message deduplication
-return_error_after_even_message = False
 
 
 def get_response(
@@ -52,14 +53,14 @@ def get_response(
         response['message'] = msg
 
     # TODO: Print endpoint and method
-    logger.debug(f'Response: {response} Status code: {status_code}')
+    LOGGER.debug(f'Response: {response} Status code: {status_code}')
 
     return flask.jsonify(response), status_code
 
 
 def get_error_response(msg: str, status_code: int = 500, **kwargs) -> flask.Response:
     """Log stacktrace with error before the response"""
-    logger.exception(msg)
+    LOGGER.exception(msg)
     return get_response(status=ResponseStatus.error, status_code=status_code, msg=msg, **kwargs)
 
 
@@ -72,7 +73,7 @@ def check_health() -> flask.Response:
 @app.route("/", methods=['GET'])
 def get_messages() -> flask.Response:
     """Get all messages from the queue"""
-    logger.info('Get all messages ...')
+    LOGGER.info('Get all messages ...')
     return get_response(status=ResponseStatus.ok, messages=messages_queue)
 
 
@@ -93,7 +94,7 @@ def try_add_from_buffer() -> None:
             # Next message is not yet in the buffer, all further messages can be skipped
             break
 
-        logger.info(f'[id={message_id}] Move message from buffer into queue ...')
+        LOGGER.info(f'[id={message_id}] Move message from buffer into queue ...')
         messages_queue.append(message)
         del messages_buffer[message_id]
 
@@ -105,20 +106,20 @@ def handle_message(message_id: int, message: str) -> None:
         # Deduplication
         is_duplicated = message_id <= len(messages_queue)
         if is_duplicated:
-            logger.warning(f'[id={message_id}] Message with such id already exists!')
+            LOGGER.warning(f'[id={message_id}] Message with such id already exists!')
             return
 
         if is_next_message(message_id):
-            logger.info(f'[id={message_id}] Add new message into queue ...')
+            LOGGER.info(f'[id={message_id}] Add new message into queue ...')
             messages_queue.append(message)
             try_add_from_buffer()
         else:
             # Add into buffer since there are delayed messages
-            logger.info(f'[id={message_id}] Add new message into buffer ...')
+            LOGGER.info(f'[id={message_id}] Add new message into buffer ...')
             messages_buffer[message_id] = message
 
-        logger.info(f'[id={message_id}] There are {len(messages_queue)} messages in queue.')
-        logger.info(f'[id={message_id}] There are {len(messages_buffer)} messages in buffer.')
+        LOGGER.info(f'[id={message_id}] There are {len(messages_queue)} messages in queue.')
+        LOGGER.info(f'[id={message_id}] There are {len(messages_buffer)} messages in buffer.')
 
 
 @app.route("/", methods=['POST'])
@@ -147,40 +148,40 @@ def append_message() -> flask.Response:
     if len(message_ids) != len(messages):
         return get_error_response('Number of messages is not the same as number of ids!')
 
-    if post_delay > 0:
-        logger.info(f'Sleep for {post_delay} seconds ...')
-        time.sleep(post_delay)
+    if POST_DELAY > 0:
+        LOGGER.info(f'Sleep for {POST_DELAY} seconds ...')
+        time.sleep(POST_DELAY)
 
-    if return_error_before_even_message and sum(message_ids) % 2 == 0:
+    if RETURN_ERROR_BEFORE_EVEN_MESSAGE and sum(message_ids) % 2 == 0:
         # Error is returned if sum(message_id) is even and messages are not added into the queue
         return get_error_response(msg='Testing error before even message!')
 
-    logger.info(f'Will try to add {len(messages)} messages ...')
+    LOGGER.info(f'Will try to add {len(messages)} messages ...')
     for message_id, message in sorted(zip(message_ids, messages)):
         handle_message(message_id, message)
 
     # Error is returned if sum(message_id) is even after messages were added into the queue
-    status_code = 500 if return_error_after_even_message and sum(message_ids) % 2 == 0 else 200
+    status_code = 500 if RETURN_ERROR_AFTER_EVEN_MESSAGE and sum(message_ids) % 2 == 0 else 200
 
     return get_response(status=ResponseStatus.ok, status_code=status_code)
 
 
 def run_app() -> None:
-    global post_delay, return_error_before_even_message, return_error_after_even_message, logger
+    global POST_DELAY, RETURN_ERROR_BEFORE_EVEN_MESSAGE, RETURN_ERROR_AFTER_EVEN_MESSAGE, LOGGER
 
     host = os.environ['SECONDARY_HOST']
     port = int(os.environ['SECONDARY_PORT'])
-    post_delay = int(os.getenv('POST_DELAY', 0))
-    return_error_before_even_message = bool(os.getenv('ERROR_BEFORE_EVEN_MESSAGE', 'false') == 'true')
-    return_error_after_even_message = bool(os.getenv('ERROR_AFTER_EVEN_MESSAGE', 'false') == 'true')
+    POST_DELAY = int(os.getenv('POST_DELAY', 0))
+    RETURN_ERROR_BEFORE_EVEN_MESSAGE = bool(os.getenv('ERROR_BEFORE_EVEN_MESSAGE', 'false') == 'true')
+    RETURN_ERROR_AFTER_EVEN_MESSAGE = bool(os.getenv('ERROR_AFTER_EVEN_MESSAGE', 'false') == 'true')
 
     app.run(host=host, port=port, debug=False)
 
 
 def main() -> None:
-    global logger
+    global LOGGER
 
-    logger = get_console_logger(service_name, level=logging.DEBUG)
+    LOGGER = get_console_logger(SERVICE_NAME, level=logging.DEBUG)
     run_app()
 
 
